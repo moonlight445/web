a8ce8640dfae63648f088c0f5d53ad4dfe8c8556
kIwwwww1 <danil29070@gmail.com>
2026-06-05 00:01:22 +0300

    import app

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,10 @@
 from fastapi.middleware.cors import CORSMiddleware
 
+from .biba import app
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"], # В продакшене укажите адрес вашего фронтенда
+    allow_origins=["*"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
